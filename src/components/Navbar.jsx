1f1cf3625152f0c3c81fa5b45126e46b7750b27f
AndySmithQA <andy.smith@qa.com>
2024-07-26 07:32:55 +0100

    Final Edits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ export default function Navbar(){
             <a className="navbar-brand p-2" href="#">
             <img src={skoda} width="100"  alt="" />
         </a>
+        <h2 class="reg">AP72 HZZ</h2>
         <span id="title" className="lead fw-bold px-4 skoda-custom">Kodiaq Milage Tracking</span>
     </nav>
     
